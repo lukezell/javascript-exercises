aeca9e8baf40818efe73cbb8df58b73adbb00896
lukezell <47638095+lukezell@users.noreply.github.com>
2025-04-11 03:10:11 -0500

    Complete exercise 4 - Remove From Array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(givenArray, arg1, arg2, arg3, arg4) {
+    let newArray = [];
+    function buildAnArray(value) {
+        if (value !== arg1 && value !== arg2 && value !== arg3 && value !== arg4) {
+            newArray.push(value);
+        }
+    }
+
+    givenArray.forEach(buildAnArray);
+
+    return newArray;
 };
+
+removeFromArray([1, 2, 3, 4], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
